ea6f7d699ce61898181130deb5a27bdc810b76f3
simoelie <148824320+simoelie@users.noreply.github.com>
2026-09-20 06:48:08 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE="job-agent-v5";self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["/","/static/app.css"]))));self.addEventListener("fetch",e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
